dd1f02841ae428a566b231da7531eead8e075673
crypt0 <acvjetan@gmail.com>
2020-12-20 23:07:20 +0100

    Add checkboxes.

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -10,11 +10,17 @@
       <option value="developer">Web Developer</option>
       <option value="designer">Web Designer</option>
     </select>
+
+    <div class="terms">
+      <input type="checkbox" required v-model="terms" />
+      <label>Accept terms and conditions</label>
+    </div>
   </form>
 
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
+  <p>Terms: {{ terms }}</p>
 </template>
 
 <script>
@@ -24,6 +30,8 @@ export default {
       email: "",
       password: "",
       role: "",
+      terms: false,
+      names: [],
     };
   },
 };
@@ -58,5 +66,13 @@ select {
   border: none;
   border-bottom: 1px solid #ddd;
   color: #555;
+}
+
+input[type="checkbox"] {
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
