043076f491f7dc0b355b13c0a9199eeb34fb5761
crypt0 <acvjetan@gmail.com>
2020-12-20 23:18:58 +0100

    Keyboard events.

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -11,6 +11,12 @@
       <option value="designer">Web Designer</option>
     </select>
 
+    <label>Skills</label>
+    <input type="text" v-model="tempSkill" @keyup="addSkill" />
+    <div v-for="skill in skills" :key="skill" class="pill">
+      {{ skill }}
+    </div>
+
     <div class="terms">
       <input type="checkbox" required v-model="terms" />
       <label>Accept terms and conditions</label>
@@ -31,8 +37,19 @@ export default {
       password: "",
       role: "",
       terms: false,
-      names: [],
+      tempSkill: "",
+      skills: [],
     };
+  },
+  methods: {
+    addSkill(e) {
+      if (e.key === "Enter" && this.tempSkill) {
+        if (!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill);
+          this.tempSkill = "";
+        }
+      }
+    },
   },
 };
 </script>
